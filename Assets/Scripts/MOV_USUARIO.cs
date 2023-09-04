0beb761cde6e9911c30d3fa13f16dcd179a3459c
lucianatabassi <lutabassi@gmail.com>
2023-09-04 19:32:21 -0300

    4/9

--- Assets/Scripts/MOV_USUARIO.cs
+++ Assets/Scripts/MOV_USUARIO.cs
@@ -7,14 +7,28 @@ public class MOV_USUARIO : MonoBehaviour
     public CharacterController controller; //el character controller es un componenten de unity, con esto es mas facil hacer los movimientos
 
     public float vel = 10f;
+    public float gravedad = -9.8f;
+    public Transform groundCheck;
+    public float groundDistance = 0.3f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
         float x = Input.GetAxis("Horizontal"); //con esta linea de codigo unity ya sabe cuales son las teclas por defecto que se usan para moverse
         float z = Input.GetAxis("Vertical"); //lo de arriba
 
